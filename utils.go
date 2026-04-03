1396a03d8476525c2bc98937dfbf12f7afadb123
Зинченко Михаил <zma24u894@student.bmstu.ru>
2026-04-03 21:02:20 +0300

    Add new functions ContainsInt

--- utils.go
+++ utils.go
@@ -1,6 +1,17 @@
+//файл utils.go
+
 package utils
 
 func Contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsInt(a []int, x int) bool {
 	for _, n := range a {
 		if x == n {
 			return true
